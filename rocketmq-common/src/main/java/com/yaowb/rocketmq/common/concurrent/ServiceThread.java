29c538176dcf2bc02f0b2288ed4907a288acfb59
yaowenbinqwq <ywb992134@163.com>
2023-05-18 10:34:08 +0800

    feat: ServiceThread#waitForRunning and shutdown method.

--- rocketmq-common/src/main/java/com/yaowb/rocketmq/common/concurrent/ServiceThread.java
+++ rocketmq-common/src/main/java/com/yaowb/rocketmq/common/concurrent/ServiceThread.java
@@ -3,6 +3,8 @@ package com.yaowb.rocketmq.common.concurrent;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.concurrent.atomic.AtomicBoolean;
+
 
 /**
  * @Author yaowenbin
@@ -12,9 +14,15 @@ import lombok.extern.slf4j.Slf4j;
 @Data
 public abstract class ServiceThread implements Runnable {
 
+    // 关闭线程时，加入主线程的最大等待时间
+    private static final long JOIN_TIME = 90 * 1000;
+
     protected volatile boolean isDaemon = false;
 
+    // 当前线程是否并创建并且启动
     protected volatile boolean stopped = false;
+
+    protected volatile AtomicBoolean hasNotified = new AtomicBoolean(false);
 
     protected Thread thread;
 
@@ -34,7 +42,49 @@ public abstract class ServiceThread implements Runnable {
     }
 
 
+    public void shutdown(boolean interrupt) {
+        if (isStopped()) {
+            return;
+        }
+        // 通知线程状态已经停止，在run方法中的while语句条件的getStopped将会返回false
+        this.stopped = true;
+        log.info("shutdown thread[{}], interrupt={}", getServiceName(), interrupt);
+        try {
+            // 如果是中断参数为true，那么手动调用interrupted方法
+            if(interrupt) {
+                this.thread.interrupt();
+            }
+            long beginTime = System.currentTimeMillis();
+            // 等待线程真正执行完毕
+            if (!this.isDaemon) {
+                this.thread.join(getJoinTime());
+            }
+            long elapsedTime = System.currentTimeMillis() - beginTime;
+            log.info("join thread: [{}], elapsedTime: {}, join Time: {}", getServiceName(), elapsedTime, getJoinTime());
+        } catch (InterruptedException e) {
+            log.error("shutdown interrupted", e);
+            Thread.currentThread().interrupt();
+        }
+    }
+
+    protected void waitForRunning(long interval) {
+       synchronized (this) {
+           try {
+               wait(interval);
+           } catch (InterruptedException e) {
+               log.error("wait for running interrupted exception",e);
+               Thread.currentThread().interrupt();
+           }
+       }
+    }
+
     protected boolean isStopped() {
         return stopped;
     }
+
+    protected long getJoinTime() {
+        return JOIN_TIME;
+    }
+
+
 }
